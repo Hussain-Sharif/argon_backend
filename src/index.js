716e31aab184b5d701aba879e52742290cdd48ab
hussain-sharif <99sharif00@gmail.com>
2024-12-12 14:56:53 +0530

    verifiying the db connection and changes for considering the .db file in production

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ dotenv.config()  // .env config
 const __filename = fileURLToPath(import.meta.url);  // due to using ES module we get the url for this current file
 const __dirname = path.dirname(__filename); // we get the folder url for this current file
 
-const dbPath = path.join(__dirname, 'argonDb.db'); // assigned here buddy!
+const dbPath = path.join(__dirname, 'db/argonDb.db'); // assigned here buddy!
 console.log(dbPath)
 let db=null
 let port=process.env.PORT || 7000
@@ -24,6 +24,7 @@ const initializeDBAndServer = async () => {
             filename: dbPath,
             driver: sqlite3.Database,
         });
+        console.log('Database connection established');
         app.listen(port, () => {
             console.log(`Server Running at http://localhost:${port}/`);
         });
